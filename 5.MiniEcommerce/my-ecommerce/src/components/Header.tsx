2a047c8ef978405c364afcae90576e08ff0e5a0e
bngams <n.boris@devidia.net>
2025-03-07 12:14:32 +0100

    app structure with first styles on Header component

--- 5.MiniEcommerce/my-ecommerce/src/components/Header.tsx
+++ 5.MiniEcommerce/my-ecommerce/src/components/Header.tsx
@@ -1,14 +1,21 @@
 import { mainNavItems } from "@/routes";
+import { House } from "lucide-react";
 import { Link } from "react-router";
 
 function Header() {
     return (
-      <header>
+      <header className="flex justify-between items-center p-4 mb-4 bg-gray-200">
+        <div>
+            <Link to="/home" className="flex items-center space-x-2">
+                <House size={32} />
+                <span className="text-[32px] font-bold">My brand</span>
+            </Link>
+        </div>
         <nav>
-            <ul>
+            <ul className="flex space-x-4">
                 {mainNavItems.map((item, index) => (
                     <li key={index}>
-                        <Link to={item.path}>{item.label}</Link>
+                        <Link className="text-[25px]" to={item.path}>{item.label}</Link>
                     </li>
                 ))}
             </ul>
